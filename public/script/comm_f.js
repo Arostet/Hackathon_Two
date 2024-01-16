923be11c10f70adecad379615cd651d46e537eb3
Arostet <aron.stettin@gmail.com>
2024-01-16 14:35:36 +0200

    forum render

--- public/script/comm_f.js
+++ public/script/comm_f.js
@@ -3,14 +3,18 @@ const container = document.getElementById("container");
 const getMessages = async () => {
   const response = await fetch("http://localhost:3001/messages");
   const data = response.json();
-  console.log(data);
+  render(data);
 };
-getMessages();
-// setInterval(getMessages, 5000);
+// getMessages();
+setInterval(getMessages, 5000);
 
 const render = (data) => {
-  data.forEach((item) => {
-    const div = document.createElement("div");
-    div.textContent = ``;
+  data.then((res) => {
+    let html = "";
+    res.forEach((item) => {
+      html += `    <div>${item.username} says ${item.message}.</div>
+        <div> Workout: ${item.bodyparts} at ${item.time}</div>`;
+    });
+    container.innerHTML = html;
   });
 };
